be120525c4ee123ee181264d7c512c3d72e20856
baiwenhao <149799880@qq.com>
2019-07-29 16:50:26 +0800

    微博数据模型

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,4 +57,11 @@ class User extends Authenticatable
         $hash = md5(strtolower(trim($this->attributes['email'])));
         return "http://www.gravatar.com/avatar/$hash?s=$size";
     }
+
+    //指名一个用户拥有多条微博
+    public function statuses()
+    {
+        return $this->hasMany(Status::class);
+    }
+
 }
